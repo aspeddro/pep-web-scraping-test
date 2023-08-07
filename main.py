6bbbcc8893e53c399ae95595836896b0e9de0f44
Pedro Castro <aspeddro@gmail.com>
2023-08-07 18:42:08 -0300

    test without sandbox arg

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main(
         prefs,
     )
 
-    options.add_argument("--no-sandbox")
+    # options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--crash-dumps-dir=/tmp")
